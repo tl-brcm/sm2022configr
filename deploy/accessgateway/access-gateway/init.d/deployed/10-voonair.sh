0f7c4a735ec811e5bbbd11807d4abd0e86256f93
tonyliang <tony.liang@broadcom.com>
2024-04-30 00:30:18 +0800

    updated sed -i

--- deploy/accessgateway/access-gateway/init.d/deployed/10-voonair.sh
+++ deploy/accessgateway/access-gateway/init.d/deployed/10-voonair.sh
@@ -16,4 +16,4 @@ sed -i '/\/Contexts/i \
 		</Context>' /opt/CA/secure-proxy/proxy-engine/conf/server.conf
 
 # Enable the az service 
-sed '/<Context name="Authentication\/Authorization web services"/,/<\/Context>/ s/enable="no"/enable="yes"/' /opt/CA/secure-proxy/proxy-engine/conf/server.conf
+sed -i '/<Context name="Authentication\/Authorization web services"/,/<\/Context>/ s/enable="no"/enable="yes"/' /opt/CA/secure-proxy/proxy-engine/conf/server.conf
